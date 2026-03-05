3db5cd3305c2bbbc1bd5388bce6d8389b5cfe94f
Chaulin011 <147333601+Chaulin011@users.noreply.github.com>
2026-03-05 13:38:59 -0300

    sim

--- questoes/01/index.php
+++ questoes/01/index.php
@@ -1,8 +1,9 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -16,10 +17,24 @@
 
     <main>
 
-    <!-- implementação da solução -->
-     
+        <form method="get">
+            <label>Digite um valor:</label>
+            <input type="number" name="valor" required>
+            <button type="submit">Calcular</button>
+        </form>
+
+        <?php
+            if (isset($_GET["valor"])) {
+                $valor = $_GET["valor"];
+                $antecessor = $valor - 1;
+
+                echo "<p>O antecessor de $valor é $antecessor</p>";
+            }
+        ?>
+
     </main>
+
 </body>
 
-
 </html>
+```
--- questoes/05/index.php
+++ questoes/05/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -10,16 +10,58 @@
 
 <body>
 
-    <header>
-        <h2>Questão 05: Maior de Três Números</h2>
-    </header>
+<header>
+    <h2>Questão 05: Maior de Três Números</h2>
+</header>
 
-    <main>
+<main>
 
-    <!-- implementação da solução -->
-     
-    </main>
+<form method="post">
+
+    <label>Primeiro número:</label>
+    <input type="number" name="n1" required>
+
+    <br><br>
+
+    <label>Segundo número:</label>
+    <input type="number" name="n2" required>
+
+    <br><br>
+
+    <label>Terceiro número:</label>
+    <input type="number" name="n3" required>
+
+    <br><br>
+
+    <input type="submit" value="Verificar maior número">
+
+</form>
+
+<?php
+
+if(isset($_POST["n1"], $_POST["n2"], $_POST["n3"])){
+
+    $n1 = $_POST["n1"];
+    $n2 = $_POST["n2"];
+    $n3 = $_POST["n3"];
+
+    $maior = $n1;
+
+    if($n2 > $maior){
+        $maior = $n2;
+    }
+
+    if($n3 > $maior){
+        $maior = $n3;
+    }
+
+    echo "<p>O maior número é: <strong>$maior</strong></p>";
+}
+
+?>
+
+</main>
+
 </body>
-
 
 </html>
